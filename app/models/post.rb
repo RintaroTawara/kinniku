23b4a11f1e80b06d76664ad7b667894387dce450
RintaroTawara <rintarou.54@gmail.com>
2020-02-16 18:44:55 +0900

    annotateの導入#31

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :bigint           not null, primary key
+#  content     :text(65535)      not null
+#  likes_count :integer
+#  title       :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  image_id    :string(255)
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+
 class Post < ApplicationRecord
   acts_as_taggable
   validates :title, presence: true, length: { maximum: 30 }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  birthday               :date
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  gender                 :string(255)
+#  place                  :string(255)
+#  provider               :string(255)
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  role                   :integer          default("general"), not null
+#  uid                    :string(255)
+#  username               :string(255)      not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  image_id               :string(255)
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
